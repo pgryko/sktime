b9470a6a0ee3b49fa60a791ceb67adaa3cfb9e7b
Piotr Gryko <piotr.gryko@gmail.com>
2023-07-23 15:39:45 +0200

    Moved torch base class into networks/base

--- sktime/forecasting/deep_learning.py
+++
@@ -1,5 +0,0 @@
-from abc import ABC
-
-
-class BaseDeepNetworkPyTorch(ABC):
-    """Abstract base class for deep learning networks using pytorch."""
--- sktime/forecasting/ltsf.py
+++ sktime/forecasting/ltsf.py
@@ -2,7 +2,7 @@
 
 import torch
 
-from sktime.forecasting.deep_learning import BaseDeepNetworkPyTorch
+from sktime.networks.base import BaseDeepNetworkPyTorch
 
 
 class LTSFLinearForecaster(BaseDeepNetworkPyTorch):
